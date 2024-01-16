8c77dfee3e588c46b1691824e6482951382df5f3
pchloe02 <146726419+pchloe02@users.noreply.github.com>
2024-01-16 11:21:08 +0100

    modif css & script quote

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,22 +1,38 @@
-let pElement = document.querySelector('#quote')
-let pElement2 = document.querySelector('#author')
+let pElement = document.getElementById("quote");
+let pElement2 = document.getElementById("author");
 
-fetch('https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote', {
-   method: 'GET',
-   headers: {
-       'x-rapidapi-host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com',
-       'x-rapidapi-key': '206f104fffmshc6de8a557ccfe51p19404fjsn8b94a624014c'
-   }
-})
-.then(response => response.json())
-.then(data => {console.log(data)
-   
-    pElement.innerHTML = data.text
-    pElement2.innerHTML = data.author
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host":
+            "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
+          "x-rapidapi-key":
+            "206f104fffmshc6de8a557ccfe51p19404fjsn8b94a624014c",
+        },
+      }
+    );
 
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-})
+    const data = await response.json();
+    console.log(data);
 
+    pElement.innerHTML = data.text || "No quote available";
+    pElement2.innerHTML = data.author || "Unknown author";
+    localStorage.setItem("sentences", pElement.innerHTML);
+    localStorage.setItem("author", pElement2.innerHTML);
+    localStorage.setItem("updateDate", currentdate);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
-
-
+if (localStorage.updateDate !== currentdate) {
+  fetchData();
+}
